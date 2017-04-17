2e2144297fd4e4ea5fcbf6601c66f5c7cc686436
jianglu0924 <jianglu0924@163.com>
2017-04-17 18:49:49 +0800

    项目终于跑通了0.0

--- maoke/src/main/java/com/maokeshop/controller/BrandController.java
+++ maoke/src/main/java/com/maokeshop/controller/BrandController.java
@@ -2,6 +2,7 @@ package com.maokeshop.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.maokeshop.service.BrandService;
 
@@ -10,4 +11,10 @@ public class BrandController {
 	
 	@Autowired
 	private BrandService brandService;
+	
+	@RequestMapping("/toItemIndex")
+	public String toItemIndex(){
+		
+		return "item/index";
+	}
 }
